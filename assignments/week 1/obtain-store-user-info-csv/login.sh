aef6d1850d9f676f9fe3f568eae08add315ce394
Victoria Konkiel <victoriakonkiel@Victorias-Air.hsd1.mn.comcast.net>
2022-09-22 18:02:27 -0500

    messing with the if statement on login.sh

--- assignments/week 1/obtain-store-user-info-csv/login.sh
+++ assignments/week 1/obtain-store-user-info-csv/login.sh
@@ -4,11 +4,12 @@ echo "Please Log-In:";
 read -p 'Username: ' uname;
 read -sp 'Password: ' password;
 
-if [ $password -eg sh parser.sh /persist-data-exercise.csv | grep "Password: ${password}";
+if [ $password | awk -F ',' -v var="$password" '$5 ];
 then
    echo "Success!";
 else
    echo "Oops! Your password is incorrect. Please try again.";
+fi
 #sh parser.sh /persist-data-exercise.csv | grep -A4 "Password: ${password};
 
 
