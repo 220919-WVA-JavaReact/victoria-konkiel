5f63d40117c4a18e7ba78e99fcb0997cd250a3b0
Victoria Konkiel <victoriakonkiel@Victorias-Air.hsd1.mn.comcast.net>
2022-09-28 15:16:54 -0500

    my completed assignment in SQL and not whatever language TextEdit put it into

--- assignments/week 2/multiplicity-joins-exercise.sql
+++ assignments/week 2/multiplicity-joins-exercise.sql
@@ -1,219 +1,82 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2639
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Bold;\f1\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red115\green158\blue202;\red170\green170\blue170;\red158\green158\blue158;
-\red193\green170\blue108;\red192\green192\blue192;\red238\green204\blue100;\red202\green197\blue128;\red115\green158\blue202;
-\red170\green170\blue170;\red158\green158\blue158;\red192\green192\blue192;\red238\green204\blue100;}
-{\*\expandedcolortbl;;\csgenericrgb\c45098\c61961\c79216;\csgenericrgb\c66667\c66667\c66667;\csgenericrgb\c61961\c61961\c61961;
-\csgenericrgb\c75686\c66667\c42353;\csgenericrgb\c75294\c75294\c75294;\csgenericrgb\c93333\c80000\c39216;\csgenericrgb\c79216\c77255\c50196;\csgenericrgb\c45098\c61961\c79216;
-\csgenericrgb\c66667\c66667\c66667;\csgenericrgb\c61961\c61961\c61961;\csgenericrgb\c75294\c75294\c75294;\csgenericrgb\c93333\c80000\c39216;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+create table students (
+	stu_id serial primary key,
+	"first" varchar(25),
+	"last" varchar(25),
+	phone int,
+	email varchar(25)
+);
 
-\f0\b\fs24 \cf2 create
-\f1\b0 \cf3  
-\f0\b \cf2 table
-\f1\b0 \cf3  \cf4 students\cf3  (\cf0 \
-\cf3 	\cf4 stu_id\cf3  \cf4 serial\cf3  
-\f0\b \cf2 primary
-\f1\b0 \cf3  
-\f0\b \cf2 key
-\f1\b0 \cf3 ,\cf0 \
-\cf3 	\cf5 "first"\cf3  
-\f0\b \cf5 varchar
-\f1\b0 \cf3 (\cf6 25\cf3 ),\cf0 \
-\cf3 	\cf5 "last"\cf3  
-\f0\b \cf5 varchar
-\f1\b0 \cf3 (\cf6 25\cf3 ),\cf0 \
-\cf3 	\cf4 phone\cf3  
-\f0\b \cf5 int
-\f1\b0 \cf3 ,\cf0 \
-\cf3 	\cf4 email\cf3  
-\f0\b \cf5 varchar
-\f1\b0 \cf3 (\cf6 25\cf3 )\cf0 \
-\cf3 )\cf7 ;\cf0 \
-\
+create table teachers (
+	teach_id serial primary key,
+	"first" varchar(25),
+	"last" varchar(25),
+	phone int,
+	email varchar(25)
+);
 
-\f0\b \cf2 create
-\f1\b0 \cf3  
-\f0\b \cf2 table
-\f1\b0 \cf3  \cf4 teachers\cf3  (\cf0 \
-\cf3 	\cf4 teach_id\cf3  \cf4 serial\cf3  
-\f0\b \cf2 primary
-\f1\b0 \cf3  
-\f0\b \cf2 key
-\f1\b0 \cf3 ,\cf0 \
-\cf3 	\cf5 "first"\cf3  
-\f0\b \cf5 varchar
-\f1\b0 \cf3 (\cf6 25\cf3 ),\cf0 \
-\cf3 	\cf5 "last"\cf3  
-\f0\b \cf5 varchar
-\f1\b0 \cf3 (\cf6 25\cf3 ),\cf0 \
-\cf3 	\cf4 phone\cf3  
-\f0\b \cf5 int
-\f1\b0 \cf3 ,\cf0 \
-\cf3 	\cf4 email\cf3  
-\f0\b \cf5 varchar
-\f1\b0 \cf3 (\cf6 25\cf3 )\cf0 \
-\cf3 )\cf7 ;\cf0 \
-\
+create table topics (
+	top_id serial primary key,
+	topic varchar(25)
+);
 
-\f0\b \cf2 create
-\f1\b0 \cf3  
-\f0\b \cf2 table
-\f1\b0 \cf3  \cf4 topics\cf3  (\cf0 \
-\cf3 	\cf4 top_id\cf3  \cf4 serial\cf3  
-\f0\b \cf2 primary
-\f1\b0 \cf3  
-\f0\b \cf2 key
-\f1\b0 \cf3 ,\cf0 \
-\cf3 	\cf4 topic\cf3  
-\f0\b \cf5 varchar
-\f1\b0 \cf3 (\cf6 25\cf3 )\cf0 \
-\cf3 )\cf7 ;\cf0 \
-\
+create table courses (
+	cour_num serial primary key,
+	title varchar(25),
+	duration varchar(25),
+	grade_level int,
+	instructor int references teachers unique
+);
 
-\f0\b \cf2 create
-\f1\b0 \cf3  
-\f0\b \cf2 table
-\f1\b0 \cf3  \cf4 courses\cf3  (\cf0 \
-\cf3 	\cf4 cour_num\cf3  \cf4 serial\cf3  
-\f0\b \cf2 primary
-\f1\b0 \cf3  
-\f0\b \cf2 key
-\f1\b0 \cf3 ,\cf0 \
-\cf3 	\cf4 title\cf3  
-\f0\b \cf5 varchar
-\f1\b0 \cf3 (\cf6 25\cf3 ),\cf0 \
-\cf3 	\cf4 duration\cf3  
-\f0\b \cf5 varchar
-\f1\b0 \cf3 (\cf6 25\cf3 ),\cf0 \
-\cf3 	\cf4 grade_level\cf3  
-\f0\b \cf5 int
-\f1\b0 \cf3 ,\cf0 \
-\cf3 	\cf4 instructor\cf3  
-\f0\b \cf5 int
-\f1\b0 \cf3  
-\f0\b \cf2 references
-\f1\b0 \cf3  \cf4 teachers\cf3  \cf0 \
-\cf3 )\cf7 ;\cf0 \
-\
+create table student_courses (
+	stu_id int references students,
+	cour_num int references courses
+);
 
-\f0\b \cf2 create
-\f1\b0 \cf3  
-\f0\b \cf2 table
-\f1\b0 \cf3  \cf4 student_courses\cf3  (\cf0 \
-\cf3 	\cf4 stu_id\cf3  
-\f0\b \cf5 int
-\f1\b0 \cf3  
-\f0\b \cf2 references
-\f1\b0 \cf3  \cf4 students\cf3 ,\cf0 \
-\cf3 	\cf4 cour_num\cf3  
-\f0\b \cf5 int
-\f1\b0 \cf3  
-\f0\b \cf2 references
-\f1\b0 \cf3  \cf4 courses\cf0 \
-\cf3 )\cf7 ;\cf0 \
-\
+create table course_topics (
+	cour_num int references courses,
+	top_id int references topics
+);
 
-\f0\b \cf2 create
-\f1\b0 \cf3  
-\f0\b \cf2 table
-\f1\b0 \cf3  \cf4 course_topics\cf3  (\cf0 \
-\cf3 	\cf4 cour_num\cf3  
-\f0\b \cf5 int
-\f1\b0 \cf3  
-\f0\b \cf2 references
-\f1\b0 \cf3  \cf4 courses\cf3 ,\cf0 \
-\cf3 	\cf4 top_id\cf3  
-\f0\b \cf5 int
-\f1\b0 \cf3  
-\f0\b \cf2 references
-\f1\b0 \cf3  \cf4 topics\cf0 \
-\cf3 )\cf7 ;\cf0 \
-\
+insert into students ("first", "last", phone, email)
+values
+	('Wanda', 'Maximoff', 5555555, 'wmax@example.com'),
+	('Jane', 'Foster', 5555556, 'jfost@ecample.com'),
+	('Natasha', 'Romanoff', 5555557, 'nrom@example.com'),
+	('Shuri', null, 5555558, 'shuri@example.com'),
+	('Carol', 'Danvers', 5555559, 'cdan@example.com');
 
-\f0\b \cf2 insert
-\f1\b0 \cf3  
-\f0\b \cf2 into
-\f1\b0 \cf3  \cf4 students\cf3  (\cf5 "first"\cf3 , \cf5 "last"\cf3 , \cf4 phone\cf3 , \cf4 email\cf3 )\cf0 \
+insert into teachers ("first", "last", phone, email)
+values
+	('Leslie', 'Knope', 5555551, 'lknope@example.com'),
+	('April', 'Ludgate', 5555552, 'alud@example.com'),
+	('Donna', 'Meagle', 5555553, 'dmeag@example.com');
 
-\f0\b \cf2 values
-\f1\b0 \cf0 \
-\cf3 	(\cf8 'Wanda'\cf3 , \cf8 'Maximoff'\cf3 , \cf6 5555555\cf3 , \cf8 'wmax@example.com'\cf3 ),\cf0 \
-\cf3 	(\cf8 'Jane'\cf3 , \cf8 'Foster'\cf3 , \cf6 5555556\cf3 , \cf8 'jfost@ecample.com'\cf3 ),\cf0 \
-\cf3 	(\cf8 'Natasha'\cf3 , \cf8 'Romanoff'\cf3 , \cf6 5555557\cf3 , \cf8 'nrom@example.com'\cf3 ),\cf0 \
-\cf3 	(\cf8 'Shuri'\cf3 , 
-\f0\b \cf2 null
-\f1\b0 \cf3 , \cf6 5555558\cf3 , \cf8 'shuri@example.com'\cf3 ),\cf0 \
-\cf3 	(\cf8 'Carol'\cf3 , \cf8 'Danvers'\cf3 , \cf6 5555559\cf3 , \cf8 'cdan@example.com'\cf3 )\cf7 ;\cf0 \
-\
+insert into courses (title, duration, grade_level, instructor)
+values
+	('gym', '1 semester', 12, 1),
+	('calculus', '2 semesters', 11, 2),
+	('physics', '2 semesters', 10, 3);
 
-\f0\b \cf2 insert
-\f1\b0 \cf3  
-\f0\b \cf2 into
-\f1\b0 \cf3  \cf4 teachers\cf3  (\cf5 "first"\cf3 , \cf5 "last"\cf3 , \cf4 phone\cf3 , \cf4 email\cf3 )\cf0 \
+insert into topics (topic)
+values
+	('math'),
+	('physical education'),
+	('science')
 
-\f0\b \cf2 values
-\f1\b0 \cf0 \
-\cf3 	(\cf8 'Leslie'\cf3 , \cf8 'Knope'\cf3 , \cf6 5555551\cf3 , \cf8 'lknope@example.com'\cf3 ),\cf0 \
-\cf3 	(\cf8 'April'\cf3 , \cf8 'Ludgate'\cf3 , \cf6 5555552\cf3 , \cf8 'alud@example.com'\cf3 ),\cf0 \
-\cf3 	(\cf8 'Donna'\cf3 , \cf8 'Meagle'\cf3 , \cf6 5555553\cf3 , \cf8 'dmeag@example.com'\cf3 )\cf7 ;\cf0 \
-\
+insert into student_courses
+values
+	(1, 1),
+	(4, 1),
+	(4, 2),
+	(1, 3),
+	(2, 2),
+	(3, 2),
+	(5, 1),
+	(3, 3);
 
-\f0\b \cf2 insert
-\f1\b0 \cf3  
-\f0\b \cf2 into
-\f1\b0 \cf3  \cf4 courses\cf3  (\cf4 title\cf3 , \cf4 duration\cf3 , \cf4 grade_level\cf3 , \cf4 instructor\cf3 )\cf0 \
-
-\f0\b \cf2 values
-\f1\b0 \cf3  \cf0 \
-\cf3 	(\cf8 'gym'\cf3 , \cf8 '1 semester'\cf3 , \cf6 12\cf3 , \cf6 1\cf3 ),\cf0 \
-\cf3 	(\cf8 'calculus'\cf3 , \cf8 '2 semesters'\cf3 , \cf6 11\cf3 , \cf6 2\cf3 ),\cf0 \
-\cf3 	(\cf8 'physics'\cf3 , \cf8 '2 semesters'\cf3 , \cf6 10\cf3 , \cf6 3\cf3 )\cf7 ;\cf0 \
-\
-
-\f0\b \cf2 insert
-\f1\b0 \cf3  
-\f0\b \cf2 into
-\f1\b0 \cf3  \cf4 topics\cf3  (\cf4 topic\cf3 )\cf0 \
-
-\f0\b \cf2 values
-\f1\b0 \cf0 \
-\cf3 	(\cf8 'math'\cf3 ),\cf0 \
-\cf3 	(\cf8 'physical education'\cf3 ),\cf0 \
-\cf3 	(\cf8 'science'\cf3 )\
-\
-\pard\pardeftab720\partightenfactor0
-
-\f0\b \cf9 insert
-\f1\b0 \cf10  
-\f0\b \cf9 into
-\f1\b0 \cf10  \cf11 student_courses\cf10  \cf0 \
-
-\f0\b \cf9 values
-\f1\b0 \cf0 \
-\pard\pardeftab720\partightenfactor0
-\cf10 	(\cf12 1\cf10 , \cf12 1\cf10 ),\cf0 \
-\cf10 	(\cf12 4\cf10 , \cf12 1\cf10 ),\cf0 \
-\cf10 	(\cf12 4\cf10 , \cf12 2\cf10 ),\cf0 \
-\cf10 	(\cf12 1\cf10 , \cf12 3\cf10 ),\cf0 \
-\cf10 	(\cf12 2\cf10 , \cf12 2\cf10 ),\cf0 \
-\cf10 	(\cf12 3\cf10 , \cf12 2\cf10 ),\cf0 \
-\cf10 	(\cf12 5\cf10 , \cf12 1\cf10 ),\cf0 \
-\cf10 	(\cf12 3\cf10 , \cf12 3\cf10 )\cf13 ;\cf0 \
-\
-\pard\pardeftab720\partightenfactor0
-
-\f0\b \cf9 insert
-\f1\b0 \cf10  
-\f0\b \cf9 into
-\f1\b0 \cf10  \cf11 course_topics\cf0 \
-
-\f0\b \cf9 values
-\f1\b0 \cf0 \
-\pard\pardeftab720\partightenfactor0
-\cf10 	(\cf12 1\cf10 , \cf12 2\cf10 ),\cf0 \
-\cf10 	(\cf12 2\cf10 , \cf12 1\cf10 ),\cf0 \
-\cf10 	(\cf12 3\cf10 , \cf12 1\cf10 ),\cf0 \
-\cf10 	(\cf12 3\cf10 , \cf12 3\cf10 )\cf13 ;}
+insert into course_topics
+values
+	(1, 2),
+	(2, 1),
+	(3, 1),
+	(3, 3);
