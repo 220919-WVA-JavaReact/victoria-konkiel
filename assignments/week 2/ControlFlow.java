badae2cd6b1b8fbf6cff937fcf98408d0b42ff4d
Victoria Konkiel <victoriakonkiel@Victorias-Air.hsd1.mn.comcast.net>
2022-09-29 17:56:44 -0500

    corrected return statement, still working on fixing the error

--- assignments/week 2/ControlFlow.java
+++ assignments/week 2/ControlFlow.java
@@ -128,7 +128,7 @@ public class ControlFlow {
     public static int[] orderArr(int[] intArr) {
 
         // logic
-        // int[] sortedArr = Arrays.sort(intArr);
-        return intArr;
+        int[] sortedArr = Arrays.sort(intArr);
+        return sortedArr;
     }
 }
